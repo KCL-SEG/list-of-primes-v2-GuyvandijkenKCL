7f5aba0b98a46a47957b253e21e703666b92a79e
Guy Van Dijken <k21008676@kcl.ac.uk>
2022-10-12 15:13:04 +0100

    Fix errors

--- primes.py
+++ primes.py
@@ -1,6 +1,8 @@
 def primes(number_of_primes):
     list = [2]
     x = 3
+    if number_of_primes < 1:
+        raise ValueError("Needs to be at least 1")
     while (len(list) < number_of_primes):
         y = 0
         prime = True
@@ -12,3 +14,7 @@ def primes(number_of_primes):
             list.append(x)
         x = x + 2
     return list
+
+
+
+
