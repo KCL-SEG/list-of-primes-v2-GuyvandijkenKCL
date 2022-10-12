4980effb8d2dace71c664ffb95e32bda034ebd44
Guy Van Dijken <k21008676@kcl.ac.uk>
2022-10-12 15:06:57 +0100

    Do all

--- primes.py
+++ primes.py
@@ -1,6 +1,14 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    x = 3
+    while (len(list) < number_of_primes):
+        y = 0
+        prime = True
+        while y < len(list):
+            if x % list[y] == 0:
+                prime = False
+            y = y + 1
+        if prime == True:
+            list.append(x)
+        x = x + 2
     return list
